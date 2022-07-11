164344edaac20fff5ba6901a8e02e5d4e8649db7
marton-szabo-epam <Marton_Szabo2@epam.com>
2022-07-11 14:03:22 +0200

    first commit

---
+++ my_app/tests.py
@@ -0,0 +1,17 @@
+from django.test import SimpleTestCase
+from django.urls import reverse  
+
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):  
+        response = self.client.get(reverse("homepage"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_content(self):
+        response = self.client.get(reverse("homepage"))
+        self.assertContains(response, "Hello, World!")
+        self.assertNotContains(response, "Not on the page")
